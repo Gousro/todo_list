8d71be21fc4562737a5b68f06d85c7188a57c326
Gousro <gousro@outlook.com>
2022-08-23 17:15:07 -0300

    Main Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <Tutorial />
+  <main>
+    <h1>Quadro de Tarefas</h1>
+    <p>Crie uma lista de tarefas</p>
+
+    <div class="create-new">
+      <input
+        v-model="newTask"
+        type="text"
+        placeholder="Add uma nova tarefa"
+        @keypress.enter="addTask"
+      >
+      <button @click="addTask">
+        Add
+      </button>
+    </div>
+
+    <div class="tasks">
+      <task-component v-for="(task, i) in $store.state.tasks" :key="i" :task="task" />
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask) {
+        this.$store.commit('add_task', this.newTask)
+        this.newTask = ''
+      }
+    }
+  }
 }
 </script>
